86ab7f02e0af0e7d4e3364c43070dcd925a294ff
Crystal Norman <cnorm88@gmail.com>
2020-10-02 20:16:01 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,11 @@
 class Post
   
-  attr_accessor :title 
+  attr_accessor :title, :author
   
-  def initialize
-    @title = title
- 
-  end
+  # def initialize
+  #   @title = title
+    
+  # end
 
 
 end 
